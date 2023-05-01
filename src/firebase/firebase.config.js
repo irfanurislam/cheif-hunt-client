b0cf1645271b8d6f0a5329058bceb24b2cd5430c
irfanurislam <mdirfanurislam@gmail.com>
2023-05-02 00:46:12 +0600

    folder structure setup

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDlE4q9XxfqQVMzAfy7Mhy5-hWv_baJRGc",
+  authDomain: "cheif-hunt-auth-firebase.firebaseapp.com",
+  projectId: "cheif-hunt-auth-firebase",
+  storageBucket: "cheif-hunt-auth-firebase.appspot.com",
+  messagingSenderId: "211665503274",
+  appId: "1:211665503274:web:1b18e57a9da139e4deef53"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app
---
+++ src/pages/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div>
+           <h2>this is foter items</h2>
+        </div>
+    );
+};
+
+export default Footer;
