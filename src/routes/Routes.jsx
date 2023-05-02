8af50212affd726f4a5ec08c2a8f509a0af238ff
irfanurislam <mdirfanurislam@gmail.com>
2023-05-02 12:22:43 +0600

    error page

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -2,11 +2,13 @@ import { createBrowserRouter } from "react-router-dom";
 import LoginLayout from "../layouts/LoginLayout";
 import Login from "../pages/Login/Login";
 import Register from "../pages/Register/Register";
+import ErrorPage from "../pages/ErrorPage/ErrorPage";
 
 const router = createBrowserRouter([
     {
         path:'/',
         element:<LoginLayout></LoginLayout>,
+        errorElement:<ErrorPage></ErrorPage>,
         children: [
 
             {
