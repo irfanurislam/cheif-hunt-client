2ce7a4665d08014199633b1c26836df3a08933ec
irfanurislam <mdirfanurislam@gmail.com>
2023-05-04 19:19:55 +0600

    last

--- src/pages/Home/ChefDetails.jsx
+++ src/pages/Home/ChefDetails.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { toast } from "react-hot-toast";
-import { FaRegHeart, FaStar } from "react-icons/fa";
+import { FaRegHeart, FaStar, FaThumbsUp } from "react-icons/fa";
 import LazyLoad from "react-lazy-load";
 import { useLoaderData, useParams } from "react-router-dom";
 
@@ -10,13 +10,14 @@ const ChefDetails = () => {
   // const chefinfo = useLoaderData();
   // const [info, setInfo] = useState(chefinfo);
   console.log(info);
+  console.log(id)
 
   const {recipes, chef_name,description, years_of_experience, likes, picture_url, num_recipes } =
     info;
 
   return (
-    <div className="px-8">
-      <div className="hero min-h-screen bg-base-200">
+    <div className="px-8 lg:px-8">
+      <div className="hero min-h-screen">
         <div className="hero-content flex-col lg:flex-row-reverse">
           <LazyLoad>
           <img
@@ -24,13 +25,13 @@ const ChefDetails = () => {
             className="max-w-sm rounded-lg shadow-2xl"
           />
           </LazyLoad>
-          <div>
+          <div className="lg:ml-8">
             <h1 className="text-5xl font-bold">{chef_name}</h1>
             <p className="py-6">
              {description}
             </p>
             <p className="my-4">Number of receips: {num_recipes} items</p>
-            <p> Like: {likes}</p>
+            <p className="flex items-center"> Likes: {likes}  </p>
             <p className="my-4"> Exprience: {years_of_experience} year</p>
             <button className="btn btn-warning">Get Started</button>
           </div>
@@ -43,28 +44,28 @@ const ChefDetails = () => {
 <table className="w-full">
   <thead className="bg-primary text-white">
     <tr>
-      <th className="px-4 py-2">Recipe Name</th>
-      <th className="px-4 py-2">Ingredients</th>
-      <th className="px-4 py-2">Cooking Method</th>
-      <th className="px-4 py-2">Rating</th>
-      <th className="px-4 py-2">Action</th>
+      <th className="px-2 lg:px-4 py-2">Recipe Name</th>
+      <th className="px-2 lg:px-4 py-2">Ingredients</th>
+      <th className="px-2 lg:px-4 py-2">Cooking Method</th>
+      <th className="px-2 lg:px-4 py-2">Rating</th>
+      <th className="px-2 lg:px-4 py-2">Action</th>
     </tr>
   </thead>
   <tbody>
-    {recipes.map((recipe) => (
-      <tr key={recipe.id}>
-       <td className="border px-4 py-2">
-            <div className="flex items-center w-full">
+    {recipes.map((recipe,index) => (
+      <tr key={index}>
+       <td className="border px-2 lg:px-4 py-2">
+            <div className=" md:flex items-center w-full">
              
               <img src={recipe.photo} alt={recipe.recipe_name} className="w-12 h-16 rounded-md mr-4" />
               
               <span>{recipe.recipe_name}</span>
             </div>
           </td>
-        <td className="border px-4 py-2 font-normal">{recipe.ingredients.join(", ")}</td>
-        <td className="whitespace-pre-wrap border px-4 py-2 font-normal ">{recipe.cooking_method}</td>
-        <td className="border px-4 py-2 font-normal"><FaStar className="text-warning"></FaStar> {recipe.rating}</td>
-        <td className="border px-4 py-2">
+        <td className="border px-2 lg:px-4 py-2 font-normal">{recipe.ingredients.join(", ")}</td>
+        <td className="whitespace-pre-wrap border px-2 lg:px-4 py-2 font-normal ">{recipe.cooking_method}</td>
+        <td className="border px-2 lg:px-4 py-2 font-normal"><FaStar className="text-warning"></FaStar> {recipe.rating}</td>
+        <td className="border px-2 lg:px-4 py-2">
           <button onClick={() => {
                 toast("Favourite Items ❤️");
                 event.target.disabled = true;
@@ -88,5 +89,3 @@ const ChefDetails = () => {
 
 export default ChefDetails;
 
-// https://i.ibb.co/ysyZHKL/wxswxy1511452625.jpg
-//https://i.ibb.co/b58C66j/ustsqw1468250014.jpg
--- src/provider/AuthProvider.jsx
+++ src/provider/AuthProvider.jsx
@@ -13,7 +13,7 @@ const AuthProvider = ({children}) => {
    const [user,setUser] = useState(null)
    const [loading,setLoading] = useState(true)
 
-   console.log(user?.email)
+   // console.log(user?.email)
 
  const createUser = (email,password) =>{
     return createUserWithEmailAndPassword(auth,email,password)
